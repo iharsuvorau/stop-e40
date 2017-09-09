55a0d920420087bc6846d9a7ad977bc820b5b812
Ihar Suvorau <ihar.suvorau@gmail.com>
2017-09-09 16:47:31 +0300

    titles and leads' updates

--- src/components/Film.vue
+++ src/components/Film.vue
@@ -5,9 +5,6 @@
       <p class="col-7" v-html="content.description"></p>
     </div>
     <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/75KGIq7SToU?rel=0?ecver=2" width="640" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
-    <div class="flex flex-wrap justify-center">
-      <p class="col-7 mb0" v-for="(item, index) in content.credits" :key="index" v-html="item"></p>
-    </div>
   </section>
 </template>
 
